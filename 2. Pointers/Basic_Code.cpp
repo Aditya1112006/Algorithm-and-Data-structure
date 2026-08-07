2da5742053389a831b90499e6947793d91bb89a4
Aditya <chauhanaditya1112006@gmail.com>
2026-08-07 12:04:08 +0530

    basic code

--- 2. Pointers/Basic_Code.cpp
+++ 2. Pointers/Basic_Code.cpp
@@ -4,6 +4,14 @@ using namespace std;
 int main(){
     int a = 10;
     int* ptr = &a;
-    cout<<*ptr<<endl;
+    cout<<*ptr<<endl; // Pointer
+
+    int b = 11;
+    cout<<&b<<endl;
+    cout<<*(&b)<<endl; // Dereferencing pointer
+
+    int* pptr = NULL;
+    cout<<pptr<<endl; // Null pointer
     return 0;
 }
+
