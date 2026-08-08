8b4f17f1409af6cbfcb334db3bfc5489d99e7e5b
Aditya <chauhanaditya1112006@gmail.com>
2026-08-08 15:45:01 +0530

    Basic array code

---
+++ 3. Array/array.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the length of array : ";
+    cin>>n;
+    int arr[n];
+    // n = sizeof(arr) / sizeof(int);
+
+    for(int i = 0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    for(int i = 0;i<n;i++){
+        cout<<arr[i]<< " ";
+    }
+    cout<<endl;
+    return 0;
+}
