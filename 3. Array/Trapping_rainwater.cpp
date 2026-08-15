6a6e645ab85c507977f6f691f946288cb6feabbc
Aditya <chauhanaditya1112006@gmail.com>
2026-08-15 10:13:58 +0530

    Calculated water traped inside the bars with their height

--- 3. Array/Trapping_rainwater.cpp
+++ 3. Array/Trapping_rainwater.cpp
@@ -10,14 +10,11 @@ void trap(int *height, int n){
 
     for(int i = 1; i<n; i++){
         leftMax[i] = max(leftMax[i-1], height[i-1]);
-        cout<<leftMax[i]<<",";
     }
-    cout<<endl;
+
     for(int i = n-2; i>=0; i--){
         rightMax[i] = max(rightMax[i+1], height[i+1]);
-        cout<<rightMax[i]<< ",";
     }
-    cout<<endl;
 
     int watertrap = 0;
     for(int i =0 ;i<n; i++){
