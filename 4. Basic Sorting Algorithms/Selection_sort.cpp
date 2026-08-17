19ed883dc476a0fd3661bf38703b4c9dcf0b9939
Aditya <chauhanaditya1112006@gmail.com>
2026-08-17 09:48:32 +0530

    Selecction sort algorithm

---
+++ 4. Basic Sorting Algorithms/Selection_sort.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+void printArr(int *arr, int n){
+    for(int i = 0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+void selectionSort(int *arr, int n){
+    for(int i = 0; i<n-1;i++){
+        int minidx = i;
+        for(int j = i+1;j<n;j++){
+            if(arr[j]<arr[minidx]){
+                minidx = j;
+            }
+        }
+        swap(arr[i],arr[minidx]);
+    }
+    printArr(arr,n);
+}
+
+int main(){
+    int arr[] = {4,9,2,1,5,3};
+    int n = sizeof(arr)/sizeof(int);
+
+    selectionSort(arr,n);
+    return 0;
+}
