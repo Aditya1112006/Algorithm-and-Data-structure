bf31c286945e57c46a7311f597cf3c7c04a6c849
Aditya <chauhanaditya1112006@gmail.com>
2026-08-29 10:30:39 +0530

    Brute force approach

---
+++ 5. 2D Array/Search Matrix/Bruteforce_approach.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int searchMatrix(int mat[][4], int n, int m){
+    int key = 33;
+    for(int i = 0;i<=n;i++){
+        for(int j = 0;j<=n;j++){
+            if(mat[i][j] == key){
+                cout<<"Element found = "<< mat[i][j]<<endl;
+            }
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int matrix[4][4] = {
+        {10, 20, 30, 40},
+        {15, 25, 35, 45},
+        {27, 29, 37, 48},
+        {32, 33, 39, 50}
+    };
+
+    searchMatrix(matrix, 4,4);
+    return 0;
+}
