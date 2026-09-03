40f1e772e74f659c804bb1903b13e2bc9581e97e
Aditya <chauhanaditya1112006@gmail.com>
2026-09-03 13:17:25 +0530

    Character array along with input

--- 6. Char arr & string/char_arr.cpp
+++ 6. Char arr & string/char_arr.cpp
@@ -1,9 +1,16 @@
 #include<iostream>
+#include<cstring>
 using namespace std;
 
 
 int main(){
     char arr[5] = {'a','c','v','t','\0'}; // \0 terminates the char arr
     cout<<arr<<endl;
+
+    char sentence[50];
+    cin.getline(sentence, 50); // first argument is arr name, second argument is size of that array and the third argument is the limiter from where the arr with not take input for eg : - "." so it will stop where it will find .
+
+    cout<<"Your sentence : "<<sentence<<endl;
+    cout<<"length : "<< strlen(sentence)<<endl;
     return 0;
 }
